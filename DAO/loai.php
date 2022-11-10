756ad517137f83a162664eb806c78d3e7934cc0b
phamduc1823 <105896204+phamduc1823@users.noreply.github.com>
2022-11-11 00:35:41 +0700

    sửa loại ok

--- DAO/loai.php
+++ DAO/loai.php
@@ -8,8 +8,8 @@ function delete_loai_hang($id){
     $sql = "delete from loai_hang where ma_loai =".$id;
     pdo_execute($sql);
 }
-function update_loai_hang($ma_loai,$ten_loai){
-    $sql = "update loai_hang set ten_loai='$ten_loai' where ma_loai = ".$ma_loai;
+function update_loai($id,$name){
+    $sql = "update category set name='$name' where id = '$id'";
     pdo_execute($sql);
 }
 function load_all_loai(){
@@ -19,7 +19,7 @@ function load_all_loai(){
 }
 
 function load_one_loai($id){
-    $sql = "select * from category where id = ".$id;
+    $sql = "select * from category where id = '$id'";
     $lh = pdo_query_one($sql);
     return $lh;
 }
--- admin/index.php
+++ admin/index.php
@@ -1,42 +1,54 @@
 <?php
-    include "header.php";
-    require_once "../DAO/pdo.php";
-    require_once "../DAO/loai.php";
-    
-    //Controller
-    if(isset($_GET['act'])){
-        $act = $_GET['act'];
-        switch($act){
+include "header.php";
+require_once "../DAO/pdo.php";
+require_once "../DAO/loai.php";
+
+//Controller
+if (isset($_GET['act'])) {
+    $act = $_GET['act'];
+    switch ($act) {
             //Controller danh mục
             //add loại
-            case 'add_loai':
-                if(isset($_POST['btn-add'])){
-                    $ten_loai=$_POST['name-loai'];
-                    insert_loai_hang($ten_loai);
-                    $thong_bao = "Thêm mới thành công";
-                }
-                include "danh_muc/addloai.php";
-                break; 
+        case 'add_loai':
+            if (isset($_POST['btn-add'])) {
+                $ten_loai = $_POST['name-loai'];
+                insert_loai_hang($ten_loai);
+                $thong_bao = "Thêm mới thành công";
+            }
+            include "danh_muc/addloai.php";
+            break;
             //list loại
-            case 'list_loai':
-                $list_all_loai = load_all_loai();
-                include "danh_muc/listcategories.php";
-                break;  
+        case 'list_loai':
+            $list_all_loai = load_all_loai();
+            include "danh_muc/listcategories.php";
+            break;
             // sua loai
-            case 'sua_loai':
-                if (isset($_GET['id']) && $_GET['id'] > 0) {
-                    $id = $_GET['id'];
-                    $loai_one = load_one_loai($id);
-                }
-                include "danh_muc/editcategory.php";
-                break;  
-            //ngược lại không tồn tại act thì include "home.php"; 
-            default:
-                include "home.php";
-                break;
-        }
-    }else{
-        include "home.php";
-    }
+        case 'sua_loai':
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $id = $_GET['id'];
+                $loai_one = load_one_loai($id);
+            }
+            include "danh_muc/editcategory.php";
+            break;
+            //Cập nhật
+        case 'cap_nhat_loai':
+            if (isset($_POST['cap_nhat'])) {
+                $name = $_POST['name'];
+                $id = $_POST['id'];
+                
+                update_loai($id,$name);
+            }
 
-    include "footer.php";
+            $list_all_loai = load_all_loai();
+            include "danh_muc/listcategories.php";
+            break;
+            //ngược lại không tồn tại act thì include "home.php"; 
+        default:
+            include "home.php";
+            break;
+    }
+} else {
+    include "home.php";
+}
+
+include "footer.php";
