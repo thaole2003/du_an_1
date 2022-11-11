0a0a475b314a6a07729e9b7c7b111e5ca4c2cc18
vietnqPH27022 <105211274+vietnqPH27022@users.noreply.github.com>
2022-11-11 23:06:38 +0700

    Xoa_truyen

--- DAO/comic.php
+++ DAO/comic.php
@@ -7,11 +7,14 @@ function comic_select_all(){
     ca.name as ca_name
 from comic c
 join images i
-on c.id = i.comic_id
+on c.images_id = i.comic_id
 join category ca
-on c.id = ca.id
+on c.category_id = ca.id
     ";
-
     return pdo_query($sql);
+}
+function delete_comic($ma_comic){
+    $sql ="DELETE FROM comic where id='$ma_comic' ";
+    return pdo_execute($sql);
 }
 ?>
