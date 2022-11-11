d6baa72d7f278ca711a5c184f1975defc4a62ad2
phamduc1823 <105896204+phamduc1823@users.noreply.github.com>
2022-11-11 15:11:02 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -10,27 +10,24 @@ if (isset($_GET['act'])) {
     switch ($act) {
             //Controller danh mục
             //add loại
-
-            case 'add_loai':
-                $is_valid = true;
-                $list_loai=load_all_loai();
-                if(isset($_POST['btn-add'])){
-
-                    $ten_loai=trim($_POST['name-loai']);
-                    foreach($list_loai as $key=>$value){
-                        if($ten_loai ==""||$ten_loai==$value['name']){
-                            $thong_bao="Tên loại không được để trùng hoặc trống";
-                            $is_valid = false;
-                            break;
-                        }
+        case 'add_loai':
+            $is_valid = true;
+            $list_loai = load_all_loai();
+            if (isset($_POST['btn-add'])) {
+                $ten_loai = trim($_POST['name-loai']);
+                foreach ($list_loai as $key => $value) {
+                    if ($ten_loai == "" || $ten_loai == $value['name']) {
+                        $thong_bao = "Tên loại không được để trùng hoặc trống";
+                        $is_valid = false;
+                        break;
                     }
-                    if ($is_valid){
-                        insert_loai($ten_loai);
-                    }
-
                 }
-                include "danh_muc/addloai.php";
-                break; 
+                if ($is_valid) {
+                    insert_loai($ten_loai);
+                }
+            }
+            include "danh_muc/addloai.php";
+            break;
             //list loại
         case 'list_loai':
             $list_all_loai = load_all_loai();
@@ -50,23 +47,22 @@ if (isset($_GET['act'])) {
             if (isset($_POST['cap_nhat'])) {
                 $name = $_POST['name'];
                 $id = $_POST['id'];
-                update_loai($id,$name);
+                update_loai($id, $name);
             }
             $list_all_loai = load_all_loai();
             include "danh_muc/listcategories.php";
             //xóa loại
-            case 'xoa_loai';
-            if(isset($_GET['id'])){
+        case 'xoa_loai';
+            if (isset($_GET['id'])) {
                 $id = $_GET['id'];
                 delete_loai_hang($id);
                 $list_all_loai = load_all_loai();
                 include_once 'danh_muc/listcategories.php';
-                $thong_bao='Xóa thành công';
-
-            } 
+                $thong_bao = 'Xóa thành công';
+            }
             break;
             //load truyện
-            case 'list_truyen';
+        case 'list_truyen';
             $load_all_truyen = comic_select_all();
             include_once "../admin/truyen/comic.php";
             break;
