62ec198d11a5241da196cfadd8de4f924e5de2ab
Lee Thao <105211283+thaole2003@users.noreply.github.com>
2022-11-17 14:00:32 +0700

    comment

--- DAO/comment.php
+++ DAO/comment.php
@@ -1,11 +1,24 @@
 <?php 
- function insert_binh_luan($noi_dung,$id_user,$id_pro,$ngay_binh_luan){
-    $sql = "insert into binh_luan(noi_dung,id_user,id_pro,ngay_binh_luan) values('$noi_dung','$id_user','$id_pro','$ngay_binh_luan')";
+function insert_binh_luan($date,$detail,$comic_id,$user_id){
+    $sql = "insert into comment(date,detail,comic_id,user_id) values('$date','$detail','$comic_id','$user_id')";
+    echo $sql;
     pdo_execute($sql);
 }
 function delete_binh_luan($id){
     $sql = "delete from binh_luan where ma_binh_luan =".$id;
     pdo_execute($sql);
+}
+
+function load_all_comic_byid($id){
+$sql = "SELECT 
+c.*,
+u.`name` as u_name
+FROM comment c
+JOIN `user` u
+ON u.id = c.user_id
+ where comic_id = $id
+	order by date desc";
+    return pdo_query($sql);
 }
 
 ?>
