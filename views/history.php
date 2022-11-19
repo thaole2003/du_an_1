fdb5e7ee1fb8537f03dd4d1e627785048e8fc78c
Lee Thao <105211283+thaole2003@users.noreply.github.com>
2022-11-19 15:10:54 +0700

    Update history.php

--- views/history.php
+++ views/history.php
@@ -2,14 +2,15 @@
 <article>
      <div class="truyen">
      <h3>  <i class="fa-solid fa-flag"></i><?php echo isset($_SESSION['auth'])? "lịch sử đọc của bạn" : "bạn cần đăng nhập để xem lịch sử" ?> </h3>
- 
-<?php 
+ <div class="flex flex-wrap">
 
+
+<?php
 if(isset($_SESSION['auth'])){
     $all_history=history_comic($_SESSION['auth']['id']);
 foreach($all_history as $Key => $value){
     extract($value);?>
-    <a href="index.php?act=detail&id=<?= $id ?>">
+    <a class="" href="index.php?act=detail&id=<?= $id ?>">
     <div class="col">
         <div class="img"><img src="content/uploads/cover_img/<?= $cover_image ?>" alt="">
         </div>
@@ -23,11 +24,12 @@ foreach($all_history as $Key => $value){
         </div>
     </div>
 </a>
+
 <?php 
 }
 
 }?>
-     
+     </div>
        
     </div>
 
