c814c96abebb4ac4a2e63fe1a261fa8eca44c3f7
vietnqPH27022 <105211274+vietnqPH27022@users.noreply.github.com>
2022-11-28 23:00:52 +0700

    css admin

--- admin/user/users.php
+++ admin/user/users.php
@@ -44,8 +44,6 @@
             </tbody>
         </table>
     </div>
-    <div class="p-4">
-        <button class="bg-orange-400 hover:bg-white hover:text-orange-400 font-medium text-white p-2 px-4 rounded-md border-solid border-2 border-yellow-400"><a href="index.php?act=add_user">Thêm</a> </button>
 
     </div>
 </div>
