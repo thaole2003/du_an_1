a668083c6d8b6a8ddd6b5182efd96766a4b47fc9
phamduc1823 <105896204+phamduc1823@users.noreply.github.com>
2022-12-05 01:53:12 +0700

    full

--- views/cap_nhat_tai_khoan.php
+++ views/cap_nhat_tai_khoan.php
@@ -1,7 +1,7 @@
 <!-- content  -->
 <div class="py-4 w-full justify-center flex">
     <div class=" rounded-lg   flex flex-col w-2/3 m-4 p-4  bg-white " style="margin: 0px auto;">
-        <p class="text-center font-medium text-2xl">EDIT ACCOUNT</p>
+        <p class="text-center font-medium text-2xl">Cập nhật tài khoản</p>
         <form action="" method="POST" class="flex flex-col">
         <label class="m-2" for="">Name</label>
         <input type="text" name="name" class="email m-2 content-between  border border-soild h-8 rounded-lg w-11/12 "  value="<?= $user_id['name']?>"  placeholder="">
