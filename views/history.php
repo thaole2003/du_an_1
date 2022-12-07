2597c92a0c9e869fed3f59bbfed037b3b019fc4d
phamduc1823 <105896204+phamduc1823@users.noreply.github.com>
2022-12-07 23:17:44 +0700

    full

--- views/history.php
+++ views/history.php
@@ -6,23 +6,29 @@
             <?php
             if (isset($_SESSION['auth'])) {
                 $all_history = history_comic($_SESSION['auth']['id']);
-                foreach ($all_history as $Key => $value) {
-                    extract($value); ?>
-                    <a class="" href="index.php?act=detail&id=<?= $id ?>">
-                        <div class="col">
-                            <div class="img"><img src="content/uploads/cover_img/<?= $cover_image ?>" alt="">
-                            </div>
-                            <div class="text">
-                                <a href="#">
-                                    <h4><?= $name ?></h4>
-                                </a>
-                                <div class="ngay_update">
-                                    <h5 style="color: white;"><?= substr($date, 0, 11) ?></h5>
+                if ($all_history) {
+                    foreach ($all_history as $Key => $value) {
+                        extract($value); ?>
+                        <a class="" href="index.php?act=detail&id=<?= $id ?>">
+                            <div class="col">
+                                <div class="img"><img src="content/uploads/cover_img/<?= $cover_image ?>" alt="">
+                                </div>
+                                <div class="text">
+                                    <a href="#">
+                                        <h4><?= $name ?></h4>
+                                    </a>
+                                    <div class="ngay_update">
+                                        <h5 style="color: white;"><?= substr($date, 0, 11) ?></h5>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </a>
+                        </a>
             <?php
+                    }
+                }else{
+                    ?>
+                     <h1 class="text-center font-medium text-orange-500 p-2 px-4 text-3xl rounded">Bạn chưa đọc truyện nào</h1>
+                    <?php
                 }
             } ?>
         </div>
--- views/love.php
+++ views/love.php
@@ -5,33 +5,39 @@
         <div class="rol">
             <?php
             if (isset($_SESSION['auth'])) {
-                foreach ($love_comic as $value) {
-                    extract($value);
-                    // echo '<pre>';
-                    // print_r($value);
+                if ($love_comic) {
+                    foreach ($love_comic as $value) {
+                        extract($value);
+                        // echo '<pre>';
+                        // print_r($value);
             ?>
-                    <a href="index.php?act=detail&id=<?= $id ?>">
-                        <div class="col">
-                            <div class="product">
-                                <div class="img"><img src="content/uploads/cover_img/<?= $cover_image ?>" alt=""></div>
-                                <div class="text">
-                                    <a href="#">
-                                        <h4><?= $name ?></h4>
-                                    </a>
-                                </div>
-                                <div class="ngay_update">
-                                    <h5><?php echo substr($date, 0, 11) ?>
-                                    </h5>
+                        <a href="index.php?act=detail&id=<?= $id ?>">
+                            <div class="col">
+                                <div class="product">
+                                    <div class="img"><img src="content/uploads/cover_img/<?= $cover_image ?>" alt=""></div>
+                                    <div class="text">
+                                        <a href="#">
+                                            <h4><?= $name ?></h4>
+                                        </a>
+                                    </div>
+                                    <div class="ngay_update">
+                                        <h5><?php echo substr($date, 0, 11) ?>
+                                        </h5>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </a>
-            <?php
+                        </a>
+                    <?php
+                    }
+                } else {
+                    ?>
+                    <h1 class="text-center font-medium text-orange-500 p-2 px-4 text-3xl rounded">Bạn chưa có truyện yêu thích</h1>
+                <?php
                 }
-            }else{
+            } else {
                 ?>
                 <h1 class="h1_text">Hãy đăng nhập để có thể thêm truyện yêu thích</h1>
-                <?php
+            <?php
             }
             ?>
         </div>
